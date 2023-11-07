e9838c61bc30c11e837f99e1177c54404a6dd8b1
שפרה גרודזינצקי <326150968@mbydomain>
2023-11-07 16:16:05 +0200

    ahfjhf

--- webApi1/TaskServices.cs
+++ webApi1/TaskServices.cs
@@ -1,12 +1,45 @@
-﻿using webApi1.Models;
+﻿
+using webApi1.Models;
+using Task = webApi1.Models.Task;
 
 namespace webApi1
 {
     public class TaskServices
     {
-        public void CreatTask(Task t)
+        static List<Task> taskList = new List<Task>();
+        Task t;
+        public Task createTask(string title, string description, DateTime time, Preference preference)
         {
-            
+            t = new Task(title, description, time, preference);
+            taskList.Add(t);
+            return t;
+        }
+
+        public bool deleteTask(int id)
+        {
+            t = taskList.Find(e => e.Id == id);
+            if (t == null) return false;
+            taskList.Remove(t);
+            return true;
+        }
+
+        public Tasks UpdateTask(int id, string title, string description, DateTime time, Preference preference)
+        {
+            int i = taskList.FindIndex(e => e.Id == id);
+            if (i != -1)
+            {
+                taskList[i].Title = title;
+                taskList[i].Description = description;
+                taskList[i].Time = time;
+                taskList[i].MyPreference = preference;
+                return taskList[i];
+            }
+            else
+                return null;
+
+
+
         }
     }
+}
 }
