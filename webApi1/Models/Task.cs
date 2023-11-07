ed0a4faa1f3913bd5b6d5342d1b3c3b0908b8657
326138864 <326138864@PC5-18>
2023-11-07 16:09:28 +0200

    change 1

--- webApi1/Models/Task.cs
+++ webApi1/Models/Task.cs
@@ -1,10 +1,25 @@
-﻿namespace webApi1.Models
+﻿using System.Reflection.Metadata;
+
+namespace webApi1.Models
 {
     public class Task
     {
+        public int Id { get; } = 0;
         public string Title { get; set; }
-        public int Description { get; set; }
+        public string Description { get; set; }
         public DateTime EndDate { get; set; }
         public Preference Preference { get; set; }
+
+
+        public Task(string title, string description, DateTime time, Preference preference)
+        {
+            Id++;
+            Title = title;
+            Description = description;
+            EndDate = time;
+            Preference = preference;
+
+        }
+
     }
 }
