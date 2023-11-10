21b8fd1224934e14cc9499ba6adb5a782af5e846
326138864 <326138864@PC5-18>
2023-11-10 09:36:35 +0200

    fix

--- webApi1/Models/Task.cs
+++ webApi1/Models/Task.cs
@@ -6,5 +6,19 @@
         public int Description { get; set; }
         public DateTime EndDate { get; set; }
         public Preference Preference { get; set; }
+        
+
+
+        public Task(string title, string description, DateTime time, Preference preference)
+        {
+            Id++;
+            Title = title;
+            Description = description;
+            EndDate = time;
+            Preference = preference;
+
+        }
+
+
     }
 }
